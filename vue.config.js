b4b1a7c19b1e3fe70d695ef731329d0c9beff681
Murilo <muriferanti@hotmail.com>
2022-01-19 00:07:05 -0300

    configs de deus

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-    publicPath: '/myresume/',
-    chainWebpack: config => {
-        config
-            .plugin('html')
-            .tap(args => {
-                args[0].title = "Meu Currículo Grátis";
-                return args;
-            })
-    }
+    publicPath: process.env.NODE_ENV === 'production' ? 
+    'myresume' : '/'
 }
